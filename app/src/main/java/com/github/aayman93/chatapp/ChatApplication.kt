19f5b12bd89bb96c26eeaea1e80b449edb67002c
aayman93 <ahmedaymann93@gmail.com>
2021-10-31 20:29:04 +0200

    Initial commit

---
+++ app/src/main/java/com/github/aayman93/chatapp/ChatApplication.kt
@@ -0,0 +1,7 @@
+package com.github.aayman93.chatapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChatApplication : Application()
---
+++ app/src/main/java/com/github/aayman93/chatapp/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.github.aayman93.chatapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.aayman93.chatapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
